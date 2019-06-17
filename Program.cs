18edfcf2bda544d6e183ed7052413054f002c870
Mauricio <mauriciozx1@gmail.com>
2019-06-17 01:42:56 -0400

    BatPruebaAgregado

--- Program.cs
+++ Program.cs
@@ -12,19 +12,42 @@ namespace newapp
     {
         static void Main(string[] args)
         {
-            using (OdbcConnection db = new OdbcConnection(GetConnectionString()))
+            int stop = 1;
+            while (stop != 0)
             {
-                var queryText = QueryText();
-                var data = db.Query(queryText).ToList();
-
-                foreach (var item in data)
+                string eleccion;
+                Console.Write("Elija Opción a realizar:\n1.-Mostras tabla predeterminada de Access\n2.-Ejecutar Script FTP.\n0.-Salir\n");
+                eleccion = Console.ReadLine();
+                int opcion = Convert.ToInt32(eleccion);
+                if(opcion == 0)
                 {
-                    Console.WriteLine(item);
+                    Console.Write("Programa Terminado");
+                    System.Environment.Exit(1);               
+                }
+                else if (opcion == 1)
+                {
+                    using (OdbcConnection db = new OdbcConnection(GetConnectionString()))
+                    {
+                        var queryText = QueryText();
+                        var data = db.Query(queryText).ToList();
+
+                        foreach (var item in data)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+
+                    string GetConnectionString(string dbName = "Ergosana.mdb") => $"Driver={{Microsoft Access Driver (*.mdb, *.accdb)}};Dbq=C:\\{dbName}";
+                    string QueryText(string table = "Doctors") => $"SELECT * FROM {table}";
+                }
+                else if (opcion == 2)
+                {
+                    //Cambiar ruta segun destino de archivo.
+                    System.Diagnostics.Process.Start(@"C:\Users\Mauricio\Desktop\Repositorios\git-proyectoMetodolog\docbat\ftpconeccion.bat");
+                    Console.Write("> Ejecutando .bat < ...\n");
+                    Console.ReadLine();
                 }
             }
-
-            string GetConnectionString(string dbName = "Ergosana.mdb") => $"Driver={{Microsoft Access Driver (*.mdb, *.accdb)}};Dbq=C:\\{dbName}";
-            string QueryText(string table = "Doctors") => $"SELECT * FROM {table}";
         }
     }
 }
