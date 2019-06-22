ea44516eb8d76b72f3ca96cb6136c07a01dfbc05
Mauricio <mauriciozx1@gmail.com>
2019-06-22 18:16:06 -0400

    UltimosDetalles

--- Program.cs
+++ Program.cs
@@ -14,45 +14,73 @@ namespace newapp
         static void Main(string[] args)
         {
             int stop = 1;
-            while (stop != 0)
+            string RutaBat = @"C:\Users\Mauricio\Desktop\Repositorios\git-proyectoMetodolog\docbat\ftpconeccion.bat";
+            string RutaArchivo = @"C:\Users\Mauricio\Desktop\Repositorios\git-proyectoMetodolog\docbat\respaldo-ftp.txt";
+            string opciones = "Elija Opción a realizar:\n1.-Mostras tabla predeterminada de Access.\n2.-Generar archivo txt.\n3.-Generar coneccion a FTP.\n4.-Ejecutar todo.\n0.-Salir\n";
+            string GetConnectionString(string dbName = "Ergosana.mdb") => $"Driver={{Microsoft Access Driver (*.mdb, *.accdb)}};Dbq=C:\\{dbName}";
+            string QueryText(string table = "Doctors") => $"SELECT * FROM {table}";
+            using (OdbcConnection db = new OdbcConnection(GetConnectionString()))
             {
-                string eleccion;
-                Console.Write("Elija Opción a realizar:\n1.-Mostras tabla predeterminada de Access\n2.-Generar respaldo FTP.\n0.-Salir\n");
-                eleccion = Console.ReadLine();
-                int opcion = Convert.ToInt32(eleccion);
-                if (opcion == 1)
+                using (StreamWriter archivo = new StreamWriter(RutaArchivo))
                 {
-                    using (OdbcConnection db = new OdbcConnection(GetConnectionString()))
+                    var queryText = QueryText();
+                    var data = db.Query(queryText).ToList();
+                    while (stop != 0)
                     {
-                        using (StreamWriter archivo = new StreamWriter(@"C:\Users\Mauricio\Desktop\Repositorios\git-proyectoMetodolog\docbat\respaldo-ftp.txt"))
+
+                        string eleccion;
+                        Console.Write(opciones);
+                        eleccion = Console.ReadLine();
+                        int opcion = Convert.ToInt32(eleccion);
+                        if (opcion == 1)
                         {
+                            Console.Write("Conectando a Access ...\n");
+                            foreach (var item in data)
                             {
-                                var queryText = QueryText();
-                                var data = db.Query(queryText).ToList();
+                                Console.WriteLine(item);
+
+                            }
+                        }
+                        else if (opcion == 2)
+                        {
+
+                            {
                                 foreach (var item in data)
                                 {
                                     archivo.WriteLine(item);
-                                    Console.WriteLine(item);
 
                                 }
+                                Console.WriteLine("Archivo txt generado correctamente...");
                             }
                         }
+                        else if (opcion == 3)
+                        {
+                            //Cambiar ruta segun destino de archivo.
+                            System.Diagnostics.Process.Start(RutaBat);
+                            stop = 0;
+                            Console.Write("> Ejecutando .bat < ...\n");
+                        }
+                        else if (opcion == 4)
+                        {
+                            Console.Write("Conectando a Access ...\n");
+                            foreach (var item in data)
+                            {
+                                Console.WriteLine(item);
+                                archivo.WriteLine(item);
+                            }
+                            Console.WriteLine("Archivo txt generado correctamente...");
+                            System.Diagnostics.Process.Start(RutaBat);
+                            Console.Write("> Ejecutando .bat < ...\n");
+                            stop = 0;
+                        }
+
+                        else
+                        {
+                            stop = 0;
+                        }
                     }
-                    string GetConnectionString(string dbName = "Ergosana.mdb") => $"Driver={{Microsoft Access Driver (*.mdb, *.accdb)}};Dbq=C:\\{dbName}";
-                    string QueryText(string table = "Groups") => $"SELECT * FROM {table}";
                 }
-                else if (opcion == 2)
-                {
-                    //Cambiar ruta segun destino de archivo.
-                    System.Diagnostics.Process.Start(@"C:\Users\Mauricio\Desktop\Repositorios\git-proyectoMetodolog\docbat\ftpconeccion.bat");
-                    stop = 0;
-                    Console.Write("> Ejecutando .bat < ...\n");
-                }
-                else
-                {
-                    stop = 0;
-                }
-            }
+            }   
         }
     }
 }
